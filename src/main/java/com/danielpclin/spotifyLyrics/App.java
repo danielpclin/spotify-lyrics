9a9822d759c90f175fcb522fd5464afd7d8ac534
danielpclin <daniel.pclin@gmail.com>
2019-11-05 21:01:30 +0800

    add icon add macos build in github action

--- src/main/java/com/danielpclin/spotifyLyrics/App.java
+++ src/main/java/com/danielpclin/spotifyLyrics/App.java
@@ -3,6 +3,7 @@ package com.danielpclin.spotifyLyrics;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -19,6 +20,7 @@ public class App extends Application {
         Pane root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/css/app.css").toExternalForm());
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/spotify.png")));
         primaryStage.setScene(scene);
         primaryStage.setTitle("Spotify Lyrics");
         primaryStage.show();
